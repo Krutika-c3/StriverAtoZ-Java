56f9b1b7eab266e9b4ff65041353dd6b6f79a0cb
Krutika Chaurishiya <85577448+Krutika-c3@users.noreply.github.com>
2025-08-24 11:11:16 +0530

    Step 1: Learn the basics Lec 1: Things to Know in Java - BasicArray - DataTypes - Functions - IfElseStatement - InputOutput - Loops - SpaceComplexity - SwitchStatement - TimeComplexity

---
+++ src/main/java/com/dsa/StriverAtoZ/StriverAtoZApplication.java
@@ -0,0 +1,13 @@
+package com.dsa.StriverAtoZ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StriverAtoZApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StriverAtoZApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dsa/StriverAtoZ/StriverAtoZApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsa.StriverAtoZ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StriverAtoZApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
